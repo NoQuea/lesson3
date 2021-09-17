eadc0f01faf2b9e507766433b884114c16b3e433
Sweet <pasha_vorobyov@inbox.ru>
2021-09-17 19:07:39 +0300

    lesson 3

--- src/com/company/MinAndMax.java
+++ src/com/company/MinAndMax.java
@@ -9,18 +9,22 @@ public class MinAndMax {
         int sum = 0;
         int min = array[0];
         int max = array[0];
+        int minIndex = 0;
+        int maxIndex = 0;
 
         for (int i = 1; i < array.length; i++) {
             if(array[i] < min){
                 min = array[i];
+                minIndex = i;
             }
             else if (array[i] > max){
                 max = array[i];
+                maxIndex = i;
             }
             sum = max + min;
         }
-        System.out.println("Min value = " + min);
-        System.out.println("Max value = " + max);
+        System.out.println("Min value = " + min + " minIndex = "+ minIndex);
+        System.out.println("Max value = " + max + " maxIndex = "+ maxIndex);
         System.out.println("sum = " + sum);
 
     }
